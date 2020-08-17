d0c97a99173bb0b712dbe8c40c3017a3bdf3d791
shimahi <shimahi@paroleise.com>
2020-08-18 03:02:58 +0900

    init.sh追加

---
+++ init.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+  echo "Please set an argument"
+  return 2>&- || exit
+fi
+
+#rename this directory
+initialDir=basename$(pwd)
+initialDirVal=$(basename ${initialDir})
+mv ../${initialDirVal} ../"$1"
+cd ../"$1"
+
+## setting git
+rm -rf .git
+rm -f .gitignore README.md
+touch .gitignore .env
+echo '
+.venv/
+__pycache__/
+.DS_Store
+.vscode
+.env
+' >>.gitignore
+
+git init
+
+## write README
+touch README.md
+echo "# $1" >>README.md
+
+## remove this script
+find ./ -name "init.sh" | xargs rm
+
+poetry install
+
+docker-compose build
